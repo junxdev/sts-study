7faf44c2fc23ae7de77df334051ff3d5540dc65f
junxhome <junxdev@gmail.com>
2020-09-14 11:41:43 +0900

    1. TDD: JUnit 2. Used H2 local file database

---
+++ sts03/src/test/java/com/bit/sts03/dept/model/Dept03DaoTest.java
@@ -0,0 +1,61 @@
+package com.bit.sts03.dept.model;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Dept03DaoTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSelectAll() {
+		ApplicationContext ac = null;
+		ac = new ClassPathXmlApplicationContext("/applicationContext.xml");
+		Dept03Dao dept03Dao = (Dept03Dao) ac.getBean("dept03Dao");
+		assertNotNull(dept03Dao.selectAll()); 
+//		fail("Not yet implemented");
+	 }
+	
+	@Test
+	public void testFirst() throws SQLException {
+		ApplicationContext ac = null;
+		ac = new ClassPathXmlApplicationContext("/applicationContext.xml");
+//		Dept03Dao dept03Dao = (Dept03Dao) ac.getBean("dept03Dao");
+//		System.out.println(dept03Dao);
+		DataSource dataSource = (DataSource) ac.getBean("dataSource");
+		Connection conn = dataSource.getConnection();
+		conn.prepareStatement("CREATE TABLE `dept03` (" + 
+				"	`deptno` INT NOT NULL AUTO_INCREMENT," + 
+				"	`dname` VARCHAR(50) NULL DEFAULT NULL," + 
+				"	`loc` VARCHAR(50) NULL DEFAULT NULL," + 
+				"	PRIMARY KEY (`deptno`)\r\n" + 
+				")").execute();
+	}
+
+}
